4b10b02cb13fcae24975c3005f9cdf2152e33418
francesco <fgiananti@gmail.com>
2018-05-04 18:09:59 +0200

    added get specified id

--- routes/todo.js
+++ routes/todo.js
@@ -1,12 +1,14 @@
 const express = require('express'),
       router = express.Router(),
       bodyParser = require('body-parser'),
-      {ToDo} = require('../models/todo') // collection
+      {ToDo} = require('../models/todo'), // collection
+      {ObjectID} = require('mongodb')
 
 
 router.use(bodyParser.json());
 
 //  GET: /todos
+// esegue query database per ottenere oggetto todos (in realtà sarebbe un array ma con {} diventa oggetto)
 router.get('/', (req, res) => {
   ToDo.find().then((todos) => {
     res.send({todos});
@@ -15,9 +17,27 @@ router.get('/', (req, res) => {
   });
 });
 
-//  POST: /todos
-router.post('/', (req, res) => {
+// GET: /todos/id fetch specific todo by id
+router.get('/:id', (req, res) => {
+  // se objectid non valido
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();
+  }
 
+  ToDo.findById(req.params.id).then((todo) => {
+      // se objectid valido (formalmente) ma todo non esistente (prova a cambiare l'ultima cifra di un id)
+      if (!todo) {
+        return res.status(404).send();
+      }
+      // se tutto ok
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+//  POST: /todos
+// inviando questa request con body json con struttura come da Schema ToDo => aggiunta todo
+router.post('/', (req, res) => {
   let newTodo = new ToDo({
     text: req.body.text
   });
